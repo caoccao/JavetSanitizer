76a1c8b2df24bcf4897be83c4a3682a8ec408cbb
caoccao <caoccao@users.noreply.github.com>
2023-10-09 10:42:38 +0800

    🧪 test: Add TestJavetSanitizerExpressionSequenceChecker

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavetSanitizer
+# Javet Sanitizer
 
 Javet Sanitizer is a sanitizer for parsing and validating JavaScript code on JVM. It is built on top of [antlr4](https://github.com/antlr/antlr4) and [grammars-v4](https://github.com/antlr/grammars-v4).
 
---
+++ src/test/java/com/caoccao/javet/sanitizer/checkers/TestJavetSanitizerExpressionSequenceChecker.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023. caoccao.com Sam Cao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.caoccao.javet.sanitizer.checkers;
+
+import com.caoccao.javet.sanitizer.exceptions.JavetSanitizerException;
+import com.caoccao.javet.sanitizer.utils.SimpleList;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestJavetSanitizerExpressionSequenceChecker extends BaseTestJavetSanitizerChecker {
+    @Test
+    public void testInvalidStatements() {
+        SimpleList.of("", "   ", null).forEach(statement ->
+                assertException(
+                        () -> new JavetSanitizerExpressionSequenceChecker().check(statement),
+                        2, "The JavaScript code is empty.",
+                        null));
+        assertException(
+                () -> new JavetSanitizerExpressionSequenceChecker().check("1 +"),
+                200, "Token + is invalid. Expecting {<EOF>, ','}.",
+                "Source Code: +\n" +
+                        "Line Number: 1, 1\n" +
+                        "Column: 2, 3\n" +
+                        "Position: 2, 3");
+        assertException(
+                () -> new JavetSanitizerExpressionSequenceChecker().check("function x() {}"),
+                200, "Token x is invalid. Expecting '('.",
+                "Source Code: x\n" +
+                        "Line Number: 1, 1\n" +
+                        "Column: 9, 10\n" +
+                        "Position: 9, 10");
+        assertException(
+                () -> new JavetSanitizerExpressionSequenceChecker().check("function x(a, b) {}"),
+                200, "Token x is invalid. Expecting '('.",
+                "Source Code: x\n" +
+                        "Line Number: 1, 1\n" +
+                        "Column: 9, 10\n" +
+                        "Position: 9, 10");
+    }
+
+    @Test
+    public void testValidStatements() throws JavetSanitizerException {
+        List<String> statements = SimpleList.of(
+                "1", "'a'", "1 + 1", "a == b", "[1,2,3]", "{ a: 1, b: 2, c: 3}",
+                "() => 1", "() => {}", "(a, b) => {}", "function() {}", "function(a, b) {}");
+        for (String statement : statements) {
+            assertTrue(
+                    new JavetSanitizerExpressionSequenceChecker().check(statement),
+                    statement + " should pass.");
+        }
+    }
+}
